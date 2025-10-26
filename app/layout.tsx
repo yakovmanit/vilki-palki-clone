a9b4882f4ded6b8fb62fa4c7ba072950950c43a2
yakiv <yakov.manitskiy@gmail.com>
2025-10-27 00:54:22 +0200

    feat: add custom theme colors and integrate Google Fonts in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body>{children}</body>
+		<head>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+						rel="stylesheet" />
+			<title>Vilki Palki Clone</title>
+		</head>
+
+		<body>{children}</body>
 		</html>
 	);
 }
