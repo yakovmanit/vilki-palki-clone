d090bc99155de823e77ccbea3c260612bdb7616b
yakiv <yakov.manitskiy@gmail.com>
2025-10-26 19:45:34 +0200

    refactor: simplify HomePage component and remove unused styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next"
 import type { ReactNode } from "react"
 
-import "@app/styles/global.scss"
-
 export const metadata: Metadata = {
 	title: "Let's Develop!",
 	description: "FSD Template with Next.js by yunglocokid",
