b6120911258ec17629243d6e26e636e83edfc98c
yakiv <yakov.manitskiy@gmail.com>
2025-10-27 00:55:21 +0200

    feat: add SVG icon components for Close, Logo, and MenuArrow

---
+++ src/shared/ui/icons/MenuArrow.tsx
@@ -0,0 +1,20 @@
+export const MenuArrowIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16px"
+			height="12px"
+			fill="none"
+			viewBox="0 0 18 9"
+		>
+			<path
+				stroke="#FFBDC8"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeMiterlimit={10}
+				strokeWidth={3}
+				d="M16 2 9 7 2 2"
+			/>
+		</svg>
+	);
+};
