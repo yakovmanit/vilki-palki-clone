cdfa9bb58b86af1201d9aa493270df67e3bf54ca
yakiv <yakov.manitskiy@gmail.com>
2025-10-29 15:20:27 +0200

    feat(MenuDrawer): add desktop responsibility

--- src/shared/ui/components/MenuDrawer/MenuDrawer.tsx
+++ src/shared/ui/components/MenuDrawer/MenuDrawer.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef } from 'react';
+
+
 
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
+
+
 import { cn } from '@shared/lib';
 import { Button, CloseIcon, LogoIcon, Title } from '@shared/ui';
+
+
+
+
 
 interface Props {
 	isMenuDrawerOpen: boolean;
@@ -17,36 +27,54 @@ export const MenuDrawer: React.FC<Props> = ({
 }) => {
 	const t = useTranslations('general');
 
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+
+	}, []);
+
 	return (
 		<>
 			{/* Menu burger */}
 			<div
 				onClick={() => setIsMenuDrawerOpen(true)}
+				onMouseEnter={() => setIsMenuDrawerOpen(true)}
+				ref={ref}
 				className='-translate-x-13'
 			>
 				<LogoIcon className='fill-gray-500' />
 			</div>
 
+			<div
+				className={
+					cn('fixed w-full h-full z-10 top-0 left-0 bg-black opacity-50 transition duration-300',
+						{
+							'opacity-0 -z-10': !isMenuDrawerOpen,
+						})
+				}
+				onMouseEnter={() => setIsMenuDrawerOpen(false)}
+			/>
+
 			{/* Menu Drawer */}
 			<div
 				className={cn(
-					'pb-20 bg-white fixed z-20 top-0 right-0 w-full h-full overflow-y-auto transition duration-300 ease-in-out',
+					'pb-20 bg-white fixed z-20 top-0 left-0 w-full h-full overflow-y-auto transition duration-300 ease-in-out md:w-[500px]',
 					{
 						'transform -translate-x-full': !isMenuDrawerOpen,
 					},
 				)}
 			>
 				{/* Menu Drawer Header */}
-				<div className='bg-[#000c26] text-white flex items-center pr-4'>
+				<div className='bg-[#000c26] text-white flex items-center pr-2'>
 					<Link href='/' className='flex items-center'>
 						<LogoIcon className='fill-white' />
 
 						<p className='ml-3 uppercase font-bold text-xl'>Вилки палки</p>
 					</Link>
 
-					<div onClick={() => setIsMenuDrawerOpen(false)} className='ml-auto'>
+					<button onClick={() => setIsMenuDrawerOpen(false)} className='ml-auto cursor-pointer p-2'>
 						<CloseIcon />
-					</div>
+					</button>
 				</div>
 
 				{/* Menu Drawer Body */}
