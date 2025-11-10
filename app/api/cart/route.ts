45615395e7ea1923d115eb763f759faea265e938
yakiv <yakov.manitskiy@gmail.com>
2025-11-10 17:54:20 +0200

    feat(cart): implement GET route to retrieve user cart with items and total amount

---
+++ app/api/cart/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@prisma/prisma-client';
+import { cookies } from 'next/headers'
+
+export async function GET(req: NextRequest) {
+	try {
+		const cookieStore = await cookies()
+		const userToken = cookieStore.get('userToken')?.value;
+
+		if (!userToken) {
+			return NextResponse.json({ cartItems: [], totalAmount: 0 }, { status: 400 });
+		}
+
+		const cart = await prisma.cart.findFirst({
+			where: {
+				token: userToken
+			},
+			include: {
+				cartItems: {
+					include: {
+						product: true,
+						ingredients: true,
+					}
+				},
+			},
+		});
+
+		return NextResponse.json(cart);
+
+	} catch (err) {
+		console.log('[CART_GET] Server error', err);
+		return NextResponse.json(
+			{ message: 'Failed to get cart' },
+			{ status: 500 },
+		);
+	}
+}
