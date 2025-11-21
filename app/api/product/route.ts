ef704bfd5b626afb3ffcbd434d78d564454767f5
yakiv <yakov.manitskiy@gmail.com>
2025-11-21 02:49:11 +0200

    feat(Product Api): add price sorting

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,12 +1,13 @@
 import { prisma } from '@prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest & { categorySlug?: string; filterId?: string; price?: 'asc' | 'desc' }) {
 	try {
 		// Example:
-		// /api/product?categorySlug=sushi&filterId=1
+		// /api/product?categorySlug=sushi&filterId=1&price=desc
 		const categorySlug = req.nextUrl.searchParams.get('categorySlug');
 		const filterId = req.nextUrl.searchParams.get('filterId');
+		const price = req.nextUrl.searchParams.get('price') as 'asc' | 'desc' || 'asc';
 
 		const products = await prisma.product.findMany({
 			where: {
@@ -24,6 +25,9 @@ export async function GET(req: NextRequest) {
 						}
 					}
 				})
+			},
+			orderBy: {
+				price: price,
 			},
 			include: {
 				ingredients: true,
